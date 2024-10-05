162cf7186a7c8932f6fdff040b11e97145426357
BaekSeungChan <106970129+BaekSeungChan@users.noreply.github.com>
2024-10-05 23:39:34 +0900

    first commit

---
+++ src/main/java/com/example/hexagonalredis/HexagonalRedisApplication.java
@@ -0,0 +1,13 @@
+package com.example.hexagonalredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HexagonalRedisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hexagonalredis/HexagonalRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hexagonalredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
